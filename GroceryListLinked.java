11fcaa883941167012121611547ec2d32534dde8
JDFundadorComparativo <31430275+JDFundadorComparativo@users.noreply.github.com>
2017-09-22 14:14:39 +0800

    Add files via upload
    Grocery List implemented through link list

--- GroceryListLinked.java
+++ GroceryListLinked.java
@@ -1,4 +1,4 @@
-class GroceryListLinked implements IGroceryList {
+public class GroceryListLinked implements IGroceryList {
   
   GroceryNode head = null;
   int size = 0;
@@ -6,19 +6,80 @@ class GroceryListLinked implements IGroceryList {
   GroceryListLinked() {}
   
   public boolean add(GroceryItem item) {
-    return false;
+    GroceryNode newNode = new GroceryNode(item, head);
+    head = newNode;
+    return true;
   }
-  
+  public boolean checkHead(){
+    if(head == null)
+      return false;
+    return true;
+  }
+  public boolean checkItem(String name){//checks if item exists in the list
+    GroceryNode current = head;
+    if(current.getData().name == name)
+      return true;
+    else{
+      current = findPriorItem(name);
+      if(current != null)
+        return true;
+      else
+        return false;
+    }
+  }
+  public GroceryNode findItem(String name){
+    GroceryNode current = head;//finds the item for convenience
+    while(current != null){
+      if(current.getData().name == name)
+        return current;
+      current = current.getNext();
+    }
+    return current;
+  }
+  public GroceryNode findPriorItem(String name){
+    GroceryNode current = head;//method useful for remove coz it finds the item before the itemToBeRemoved.
+    while(current != null){
+      if(current.getNext().getData().name == name)
+        return current;
+      current = current.getNext();
+    }
+    return current;
+  }
   public boolean remove(String name) {
-    return false;
+    GroceryNode current = head;
+    if(checkHead() == false)//checking if list is empty
+      return false; 
+    
+    if(current.getData().name == name){//this is for removing the head.
+      head = current.next;
+      return true;
+      
+    
+    }else{  //this is for removing anynode in the middle.
+      if(checkItem(name)){ //checkpoint for item existence
+        current = findPriorItem(name);
+        current.setNext(current.getNext().getNext());
+        return true;
+      }
+      else return false; //this is if the item doesn't exist and returns false directly.
+    }
   }
   
   public boolean markAsBought(String name) {
+    GroceryNode current = findItem(name);
+    if(current != null){
+      current.getData().isBought = true;
+      return true;
+    }
     return false;
   }
   
   public void display() {
-    
+    GroceryNode current = head;
+    System.out.println("Grocery Items: ");
+    while(current.getNext() != null){
+      System.out.println(current.getData().name+"\n");
+    }
   }
 }
 
@@ -28,6 +89,22 @@ class GroceryNode {
   
   GroceryNode(GroceryItem data, GroceryNode next) {
     this.data = data;
+    this.next = next;
+  }
+  
+  public GroceryItem getData(){
+    return data;
+  }
+  
+  public void setData(GroceryItem data){
+    this.data = data;
+  }
+  
+  public GroceryNode getNext(){
+    return next;
+  }
+  
+  public void setNext(GroceryNode next){
     this.next = next;
   }
 }
---
+++ GroceryListLinkedTest.java
@@ -0,0 +1,76 @@
+import junit.framework.TestCase;
+
+public class GroceryListLinkedTest extends TestCase {
+  GroceryListLinked gll = new GroceryListLinked();
+    GroceryItem a = new GroceryItem("Milk", 1);
+    GroceryItem b = new GroceryItem("Sugar", 2);
+    GroceryItem c = new GroceryItem("Spice", 3);
+    GroceryItem d = new GroceryItem ("Rice", 4);
+    GroceryItem e = new GroceryItem("Meat", 5);
+    GroceryItem f = new GroceryItem("Meat", 6);
+    GroceryItem g = new GroceryItem("Potato", 7);
+    GroceryItem h = new GroceryItem("Tomato", 8);
+    GroceryItem i = new GroceryItem("Orange", 9);
+    GroceryItem j = new GroceryItem("Apple", 10);
+    GroceryItem k = new GroceryItem("Cabbage", 11);
+    GroceryItem l = new GroceryItem("Carrot", 12);
+    GroceryItem m = new GroceryItem("Soap", 13);
+    GroceryItem n = new GroceryItem("Shampoo", 14);
+   
+  public void testAddLessThanTen() {
+    assertTrue(gll.add(a));
+    assertTrue(gll.add(b));
+    assertTrue(gll.add(c));
+    assertTrue(gll.add(d));
+    assertTrue(gll.add(e));
+    assertTrue(gll.add(f));
+    assertTrue(gll.add(g));
+    assertTrue(gll.add(h));
+    assertTrue(gll.add(i));
+    assertTrue(gll.add(j)); 
+  }
+/*  public void testRemoveUnexistingItem(){ 
+    gll.add(a);
+    gll.add(b);
+    assertFalse(gll.remove(c.name));
+  }*/ 
+  public void testRemoveFromEmptyList(){
+    assertFalse(gll.remove(a.name));
+  }
+  public void testRemoveFirst(){
+    gll.add(a);
+    gll.add(b);
+    gll.add(c);
+    gll.add(d);
+    gll.add(e);
+    assertTrue(gll.remove(e.name));
+  }
+  public void testRemoveMid(){
+    gll.add(a);
+    gll.add(b);
+    gll.add(c);
+    gll.add(d);
+    gll.add(e);
+    assertTrue(gll.remove(c.name)); 
+  }
+  public void testRemoveLast(){ 
+    gll.add(a);
+    gll.add(b);
+    gll.add(c);
+    gll.add(d);
+    gll.add(e);
+    assertTrue(gll.remove(e.name));
+  }
+  public void testIsBought(){
+    gll.add(a);
+    gll.add(b);
+    gll.add(c);
+    gll.add(d);
+    gll.add(n);
+    gll.markAsBought("Milk"); 
+    gll.markAsBought("Shampoo");
+    assertTrue(a.isBought); 
+    assertFalse(b.isBought);
+    assertTrue(n.isBought);
+  }
+}
